a36003220c358994d753f7d90f2fd4f61a212e1a
Uallace Macedo <uallace.macedo@outlook.com>
2026-03-25 16:11:00 -0300

    feat: add custom errors

---
+++ java/spring-boot/spring-basic/src/main/java/com/uallace/exception/ExceptionResponse.java
@@ -0,0 +1,5 @@
+package com.uallace.exception;
+
+import java.util.Date;
+
+public record ExceptionResponse(Date timestamp, String message, String details) {}
---
+++ java/spring-boot/spring-basic/src/main/java/com/uallace/exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.uallace.exception;
+
+import com.uallace.exception.custom.UnsupportedMathOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UnsupportedMathOperation.class)
+    public ResponseEntity<ExceptionResponse> handleMathOperationExceptions(
+            Exception exception, WebRequest request
+    ) {
+        ExceptionResponse ex = new ExceptionResponse(
+                new Date(),
+                exception.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<ExceptionResponse>(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleExceptions(
+            Exception exception, WebRequest request
+    ) {
+        ExceptionResponse ex = new ExceptionResponse(
+                new Date(),
+                exception.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<ExceptionResponse>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
