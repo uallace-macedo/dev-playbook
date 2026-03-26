b7e008e5e83d977725138e7460fd5c7180d22e09
Uallace Macedo <uallace.macedo@outlook.com>
2026-03-26 09:06:06 -0300

    feat: setup

---
+++ java/spring-boot/spring-boot-jpa/src/main/java/com/uallace/exception/custom/UnsupportedOperation.java
@@ -0,0 +1,11 @@
+package com.uallace.exception.custom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedOperation extends RuntimeException {
+    public UnsupportedOperation(String message) {
+        super(message);
+    }
+}
---
+++ java/spring-boot/spring-boot-jpa/src/test/java/com/uallace/SpringBasicApplicationTests.java
@@ -0,0 +1,15 @@
+package com.uallace;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
