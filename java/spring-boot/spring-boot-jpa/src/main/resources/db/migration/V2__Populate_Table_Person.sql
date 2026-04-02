4a5d04fb2e59004e00bf1c2e4c7058bbb6447cb3
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-02 05:56:03 -0300

    feat: add flyway migrations

---
+++ java/spring-boot/spring-boot-jpa/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+    (1, 'Ayrton', 'Senna', 'Sao Paulo - Brasil', 'MALE'),
+    (2, 'Leonardo', 'da Vinci', 'Anchiano - Italy', 'MALE'),
+    (3, 'Indira', 'Ghandi', 'Sao Paulo - Brasil', 'FEMALE'),
+    (4, 'Ayrton', 'Senna', 'Sao Paulo - Brasil', 'MALE'),
+    (5, 'Ayrton', 'Senna', 'Sao Paulo - Brasil', 'MALE');
