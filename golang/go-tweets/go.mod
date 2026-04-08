34e404f26e091b6973cf214a1f53af3c3120a106
Uallace Macedo <uallace.macedo@outlook.com>
2026-04-08 19:44:19 -0300

    feat: add config

--- golang/go-tweets/go.mod
+++ golang/go-tweets/go.mod
@@ -1,3 +1,5 @@
 module github.com/uallace-macedo/dev-playbook/golang/go-tweets
 
 go 1.25.5
+
+require github.com/joho/godotenv v1.5.1
---
+++ golang/go-tweets/internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBUrlMigration string `json:"DATABASE_URL"`
+
+	DBHost     string `json:"DB_HOST"`
+	DBUser     string `json:"DB_USER"`
+	DBPassword string `json:"DB_PASSWORD"`
+	DBPort     string `json:"DB_PORT"`
+	DBName     string `json:"DB_NAME"`
+
+	APIPort      string `json:"API_PORT"`
+	APISecretJWT string `json:"API_SECRET_JWT"`
+}
+
+func LoadConfig(l *Logger) (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("Failed to load .env file\n")
+	}
+
+	l.Info("config loaded")
+
+	return &Config{
+		DBUrlMigration: os.Getenv("DATABASE_URL"),
+
+		DBHost:     os.Getenv("DATABASE_URL"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBName:     os.Getenv("DB_NAME"),
+
+		APIPort:      os.Getenv("API_PORT"),
+		APISecretJWT: os.Getenv("API_SECRET_JWT"),
+	}, nil
+}
