f45601af17af4a923a27fe84737ff09d1f348a88
Uallace Macedo <uallace.macedo@outlook.com>
2026-08-07 19:39:25 -0300

    feat(fetch): add GET /restaurants to authenticated customers

--- apps/api/vfdelivery/routes/restaurant_router.py
+++ apps/api/vfdelivery/routes/restaurant_router.py
@@ -2,12 +2,18 @@ from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 
-from vfdelivery.core.deps import LOGIN_FORM_DATA
-from vfdelivery.core.token import get_current_restaurant
+from vfdelivery.core.deps import LOGIN_FORM_DATA, SESSION
+from vfdelivery.core.token import get_current_customer, get_current_restaurant
+from vfdelivery.models.customer import Customer
 from vfdelivery.models.restaurant import Restaurant
 from vfdelivery.schemas.public_schemas import AuthToken
-from vfdelivery.schemas.restaurant_schemas import RestaurantCreate, RestaurantPublic
+from vfdelivery.schemas.restaurant_schemas import (
+    RestaurantCreate,
+    RestaurantList,
+    RestaurantPublic,
+)
 from vfdelivery.services.restaurant_service import (
     RestaurantService,
     get_restaurant_service,
@@ -16,6 +22,7 @@ from vfdelivery.services.restaurant_service import (
 router = APIRouter(prefix='/restaurants', tags=['Restaurant'])
 RESTAURANT_SERVICE = Annotated[RestaurantService, Depends(get_restaurant_service)]
 CURRENT_RESTAURANT = Annotated[Restaurant, Depends(get_current_restaurant)]
+CURRENT_USER = Annotated[Customer, Depends(get_current_customer)]
 
 
 @router.post(
@@ -36,3 +43,21 @@ def create_restaurant(data: RestaurantCreate, service: RESTAURANT_SERVICE):
 def login_restaurant(data: LOGIN_FORM_DATA, service: RESTAURANT_SERVICE):
     """Login a restaurant"""
     return service.login_restaurant(data)
+
+
+@router.get(
+    '',
+    status_code=HTTPStatus.OK,
+    response_model=RestaurantList
+)
+def get_restaurants(
+    session: SESSION,
+    current_user: CURRENT_USER,
+    limit: int = 10,
+    offset: int = 0
+):
+    restaurants = session.scalars(
+        select(Restaurant).limit(limit).offset(offset)
+    )
+
+    return RestaurantList(restaurants=restaurants)
