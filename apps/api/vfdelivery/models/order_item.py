7bc9a6b2ea263637fc0274e93b7a295b98c752d7
Uallace Macedo <uallace.macedo@outlook.com>
2026-08-09 11:23:12 -0300

    feat(models): add models

--- apps/api/vfdelivery/models/order_item.py
+++ apps/api/vfdelivery/models/order_item.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import datetime
-from enum import Enum
 
 from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
@@ -8,14 +7,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 from vfdelivery.core.database import table_registry
 
 
-class UserRole(str, Enum):
-    CUSTOMER = 'customer'
-    RESTAURANT_OWNER = 'restaurant_owner'
-
-
 @table_registry.mapped_as_dataclass
-class User:
-    __tablename__ = 'tb_users'
+class OrderItem:
+    __tablename__ = 'tb_order_items'
 
     id: Mapped[uuid.UUID] = mapped_column(
         init=False,
@@ -23,20 +17,19 @@ class User:
         default_factory=uuid.uuid4
     )
 
-    name: Mapped[str] = mapped_column(
+    order_id: Mapped[uuid.UUID] = mapped_column(
         nullable=False
     )
 
-    email: Mapped[str] = mapped_column(
-        nullable=False,
-        unique=True
-    )
-
-    password: Mapped[str] = mapped_column(
+    product_id: Mapped[uuid.UUID] = mapped_column(
         nullable=False
     )
 
-    role: Mapped[UserRole] = mapped_column(
+    quantity: Mapped[int] = mapped_column(
+        nullable=False
+    )
+
+    unit_price: Mapped[float] = mapped_column(
         nullable=False
     )
 
--- apps/api/vfdelivery/models/restaurant.py
+++ apps/api/vfdelivery/models/restaurant.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import datetime
-from enum import Enum
 
 from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
@@ -8,14 +7,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 from vfdelivery.core.database import table_registry
 
 
-class UserRole(str, Enum):
-    CUSTOMER = 'customer'
-    RESTAURANT_OWNER = 'restaurant_owner'
-
-
 @table_registry.mapped_as_dataclass
-class User:
-    __tablename__ = 'tb_users'
+class Restaurant:
+    __tablename__ = 'tb_restaurants'
 
     id: Mapped[uuid.UUID] = mapped_column(
         init=False,
@@ -23,20 +17,15 @@ class User:
         default_factory=uuid.uuid4
     )
 
+    owner_id: Mapped[uuid.UUID] = mapped_column(
+        nullable=False
+    )
+
     name: Mapped[str] = mapped_column(
         nullable=False
     )
 
-    email: Mapped[str] = mapped_column(
-        nullable=False,
-        unique=True
-    )
-
-    password: Mapped[str] = mapped_column(
-        nullable=False
-    )
-
-    role: Mapped[UserRole] = mapped_column(
+    description: Mapped[str] = mapped_column(
         nullable=False
     )
 
