7920fde03a9eeea04a709b5fc733f72c9c6583ff
Uallace Macedo <uallace.macedo@outlook.com>
2026-08-10 22:06:21 -0300

    feat(relationship): add order and order_item relationship

--- apps/api/migrations/env.py
+++ apps/api/migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from vfdelivery.core.database import table_registry
 from vfdelivery.core.settings import settings
+import vfdelivery.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
